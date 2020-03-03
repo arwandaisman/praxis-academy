13dc025e42a0e44b67d7af0a6f823a9a1b0b564c
root <root@localhost.localdomain>
2020-03-03 18:25:01 -0500

    merubah nama

---
+++ novice/01-02/latihan/5.3. tuples and sequences.py
@@ -0,0 +1,32 @@
+t = 123, 1334, 'hello!'
+t[0]
+t
+
+
+u= t,(1,2,3,4,5,6)
+u
+
+t[0] = 8888
+
+v = ([1, 2, 3], [3, 2, 1])
+
+v
+
+
+empty = ()
+singleton = 'hello',    # <-- note trailing comma
+len(empty)
+len(singleton)
+singleton
+('hello',)
+
+x,y,z = t
+
+t
+
+x
+
+y
+
+z
+
