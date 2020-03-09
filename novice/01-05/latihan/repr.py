2cebd61e977e3f24f0bb2be691d788b8474da39f
root <root@localhost.localdomain>
2020-03-09 10:38:44 +0700

    latihan

---
+++ novice/01-05/latihan/repr.py
@@ -0,0 +1,13 @@
+# input as flat text
+a =  { "Type" : "A", "field1": "value1", "field2": "value2", "field3": "value3" }
+
+# the same input can also be read from a file
+a = open('/tmp/file.py', 'r')
+
+# returns a printable representation of the input;
+# the output can be written to a file as well
+print(repr(a))
+
+# write content to files using repr
+with open('/tmp/file.py') as f:f.write(repr(a))
+
